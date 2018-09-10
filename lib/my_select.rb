5535cf057a273c7403a67baf0a6ba76c4a0b266a
Mikkel Ridley <mikkel250@gmail.com>
2018-09-10 01:04:59 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,12 +1,12 @@
 def my_select(collection)
  # your code here!
  new_coll = []
- i = 0
- while i < collection.length
+ 
+ 
   collection.each do |i|
     new_coll << i if yield i
   end
-  i = i + 1
-  end
+  
+  
   new_coll
 end
