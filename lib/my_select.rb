91f887d0064184da6241fb0a56e526c82e6488d4
Mikkel Ridley <mikkel250@gmail.com>
2018-09-10 01:03:31 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,10 @@
-def my_select(collection, &block)
+def my_select(collection)
  # your code here!
  new_coll = []
  i = 0
  while i < collection.length
-  new_i = yield collection[i]
-  
-  new_coll << (yield collection[i]) if new_i
+  collection.each do |i|
+    new_coll << (yield collection[i]) if new_i
   
   i = i + 1
   end
