9af43659fdd09af47de9560056d1f006f2912977
Mikkel Ridley <mikkel250@gmail.com>
2018-09-10 00:57:43 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,9 +4,9 @@ def my_select(collection, &block)
  i = 0
  while i < collection.length
   new_i = yield collection[i]
-  if block.call == true
-    new_coll << new_i
-  end
+  
+  new_coll << i if yield collection[i]
+  
   i = i + 1
   end
   new_coll
