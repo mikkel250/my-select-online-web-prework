7f6c229cf8c5a3af4c1ff0d23f913a77514b1044
Mikkel Ridley <mikkel250@gmail.com>
2018-09-10 00:41:18 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
  i = 0
  while i < collection.length
   new_i = yield collection[i]
-  if i == #the parameter passed to 
+  if i == (&block) 
     new_coll << new_i
   end
   i = i + 1
